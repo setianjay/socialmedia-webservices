c34923a2c306ff6ef41844f36a89bac243e3c284
Hari Setiaji <hari.setiaji2000@gmail.com>
2023-03-13 22:46:51 +0700

    Initial commit

---
+++ src/main/java/com/setianjay/socialmedia/webservices/SocialMediaWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.setianjay.socialmedia.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/setianjay/socialmediawebservices/SocialMediaWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setianjay.socialmediawebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
